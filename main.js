507067f2638876431e984f96ebb4ff3f4ad75eed
Jeroen Rijsdijk <j.rijsdijk@ma-web.nl>
2022-12-26 14:35:55 +0100

    made test function

---
+++ main.js
@@ -0,0 +1,5 @@
+var testNumber = 4;
+function addingTwoNumbers(number1, number2) {
+    return number1 + number2;
+}
+console.log(addingTwoNumbers(2, 2));
---
+++ main.ts
@@ -0,0 +1,7 @@
+let testNumber:number = 4;
+
+function addingTwoNumbers(number1:number, number2:number):number{
+    return number1 + number2;
+}
+
+console.log(addingTwoNumbers(2,2)); 
