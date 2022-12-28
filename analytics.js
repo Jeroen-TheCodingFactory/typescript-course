5c129f510074192e4875fcf9c9d5c32c345fc7b9
Jeroen Rijsdijk <j.rijsdijk@ma-web.nl>
2022-12-28 19:30:28 +0100

    made it to 36

---
+++ analytics.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("Hello I am some analytics data....");
---
+++ main.ts
@@ -0,0 +1,13 @@
+const myName = 'Jeroen';
+console.log(myName);
+
+//watch mode can be runned without --watch or --w
+
+
+// In a big project you may want to compile ALL typescript files instead of one
+// we can do this by telling typescript that this file is a typescript project
+// we have to run tsc --init 
+// thereafter we have a tsconfig.json file where we can configure the compiler
+// after that we can use tsc command to run the whole project
+// we can use watch mode with that to
+
